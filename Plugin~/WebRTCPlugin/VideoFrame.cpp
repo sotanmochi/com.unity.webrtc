2845c5cddc937742195a0cf61b32abe91c73a34b
kazuki <karasusan@gmail.com>
2022-02-06 13:21:45 +0900

    fix

--- Plugin~/WebRTCPlugin/VideoFrame.cpp
+++ Plugin~/WebRTCPlugin/VideoFrame.cpp
@@ -1,74 +1,86 @@
 #include "pch.h"
-#include "VideoFrame.h"
+
 #include "GraphicsDevice/GraphicsDevice.h"
+#include "VideoFrame.h"
 
-namespace unity {
-namespace webrtc {
+namespace unity
+{
+namespace webrtc
+{
 
-VideoFrame::VideoFrame(const Size& size,
-    std::unique_ptr<GpuMemoryBuffer> gpu_memory_buffer,
-    TimeDelta timestamp)
-    : size_(size)
-    , gpu_memory_buffer_(std::move(gpu_memory_buffer))
-    , timestamp_(timestamp)
-    {}
+    VideoFrame::VideoFrame(
+        const Size& size,
+        std::unique_ptr<GpuMemoryBuffer> buffer,
+        ReturnBufferToPoolCallback returnBufferToPoolCallback,
+        TimeDelta timestamp)
+        : size_(size)
+        , gpu_memory_buffer_(std::move(buffer))
+        , returnBufferToPoolCallback_(std::move(returnBufferToPoolCallback))
+        , timestamp_(timestamp)
+    {
+    }
 
-rtc::scoped_refptr<VideoFrame> VideoFrame::WrapExternalGpuMemoryBuffer(
-    const Size& size,
-    std::unique_ptr<GpuMemoryBuffer> gpu_memory_buffer,
-//    const gpu::MailboxHolder(&mailbox_holders)[kMaxPlanes],
-//    ReleaseMailboxAndGpuMemoryBufferCB mailbox_holder_and_gmb_release_cb,
-    TimeDelta timestamp) {
-    return new rtc::RefCountedObject<VideoFrame>(
-        size, std::move(gpu_memory_buffer), timestamp);
-}
+    VideoFrame::~VideoFrame()
+    {
+        if (returnBufferToPoolCallback_)
+        {
+            returnBufferToPoolCallback_(std::move(gpu_memory_buffer_));
+        }
+    }
 
-bool VideoFrame::HasGpuMemoryBuffer() const {
-    return gpu_memory_buffer_ != nullptr;
-}
+    rtc::scoped_refptr<VideoFrame> VideoFrame::WrapExternalGpuMemoryBuffer(
+        const Size& size,
+        std::unique_ptr<GpuMemoryBuffer> buffer,
+        ReturnBufferToPoolCallback returnBufferToPoolCallback,
+        TimeDelta timestamp)
+    {
+        return new rtc::RefCountedObject<VideoFrame>(
+            size, std::move(buffer), returnBufferToPoolCallback, timestamp);
+    }
 
-GpuMemoryBuffer* VideoFrame::GetGpuMemoryBuffer() const {
-    return gpu_memory_buffer_.get();
-}
+    bool VideoFrame::HasGpuMemoryBuffer() const { return gpu_memory_buffer_ != nullptr; }
 
-rtc::scoped_refptr<VideoFrame> VideoFrame::ConvertToMemoryMappedFrame(
-    rtc::scoped_refptr<VideoFrame> video_frame) {
-    RTC_DCHECK(video_frame);
-    RTC_DCHECK(video_frame->HasGpuMemoryBuffer());
+    GpuMemoryBuffer* VideoFrame::GetGpuMemoryBuffer() const { return gpu_memory_buffer_.get(); }
 
-    auto gmb = video_frame->GetGpuMemoryBuffer();
-    //if (!gmb->Map())
-    //    return nullptr;
+    rtc::scoped_refptr<VideoFrame>
+    VideoFrame::ConvertToMemoryMappedFrame(rtc::scoped_refptr<VideoFrame> video_frame)
+    {
+        RTC_DCHECK(video_frame);
+        RTC_DCHECK(video_frame->HasGpuMemoryBuffer());
 
-    //const size_t num_planes = VideoFrame::NumPlanes(video_frame->format());
-    //uint8_t* plane_addrs[VideoFrame::kMaxPlanes] = {};
-    //for (size_t i = 0; i < num_planes; i++)
-    //    plane_addrs[i] = static_cast<uint8_t*>(gmb->memory(i));
+        auto gmb = video_frame->GetGpuMemoryBuffer();
+        // if (!gmb->Map())
+        //    return nullptr;
 
-    //auto mapped_frame = VideoFrame::WrapExternalYuvDataWithLayout(
-    //    video_frame->layout(), video_frame->visible_rect(),
-    //    video_frame->natural_size(), plane_addrs[0], plane_addrs[1],
-    //    plane_addrs[2], video_frame->timestamp());
+        // const size_t num_planes = VideoFrame::NumPlanes(video_frame->format());
+        // uint8_t* plane_addrs[VideoFrame::kMaxPlanes] = {};
+        // for (size_t i = 0; i < num_planes; i++)
+        //    plane_addrs[i] = static_cast<uint8_t*>(gmb->memory(i));
 
-    //if (!mapped_frame) {
-    //    gmb->Unmap();
-    //    return nullptr;
-    //}
+        // auto mapped_frame = VideoFrame::WrapExternalYuvDataWithLayout(
+        //    video_frame->layout(), video_frame->visible_rect(),
+        //    video_frame->natural_size(), plane_addrs[0], plane_addrs[1],
+        //    plane_addrs[2], video_frame->timestamp());
 
-    //mapped_frame->set_color_space(video_frame->ColorSpace());
-    //mapped_frame->metadata().MergeMetadataFrom(video_frame->metadata());
+        // if (!mapped_frame) {
+        //    gmb->Unmap();
+        //    return nullptr;
+        //}
 
-    //// Pass |video_frame| so that it outlives |mapped_frame| and the mapped buffer
-    //// is unmapped on destruction.
-    //mapped_frame->AddDestructionObserver(base::BindOnce(
-    //    [](rtc::scoped_refptr<VideoFrame> frame) {
-    //        DCHECK(frame->HasGpuMemoryBuffer());
-    //        frame->GetGpuMemoryBuffer()->Unmap();
-    //    },
-    //    std::move(video_frame)));
-    //return mapped_frame;
-    return nullptr;
-}
+        // mapped_frame->set_color_space(video_frame->ColorSpace());
+        // mapped_frame->metadata().MergeMetadataFrom(video_frame->metadata());
+
+        //// Pass |video_frame| so that it outlives |mapped_frame| and the mapped buffer
+        //// is unmapped on destruction.
+        // mapped_frame->AddDestructionObserver(base::BindOnce(
+        //    [](rtc::scoped_refptr<VideoFrame> frame) {
+        //        DCHECK(frame->HasGpuMemoryBuffer());
+        //        frame->GetGpuMemoryBuffer()->Unmap();
+        //    },
+        //    std::move(video_frame)));
+        // return mapped_frame;
+        return nullptr;
+    }
 
 } // end namespace webrtc
 } // end namespace unity
--- Plugin~/WebRTCPlugin/VideoFrame.h
+++ Plugin~/WebRTCPlugin/VideoFrame.h
@@ -1,61 +1,56 @@
 #pragma once
 
-#include "rtc_base/timestamp_aligner.h"
-#include "rtc_base/ref_counted_object.h"
-#include "api/units/time_delta.h"
 #include "GpuMemoryBuffer.h"
 #include "Size.h"
+#include "api/units/time_delta.h"
+#include "rtc_base/ref_counted_object.h"
+#include "rtc_base/timestamp_aligner.h"
 
-namespace unity {
-namespace webrtc {
+namespace unity
+{
+namespace webrtc
+{
 
-using namespace ::webrtc;
+    using namespace ::webrtc;
 
-class VideoFrame : public rtc::RefCountInterface {
-public:
-    //enum {
-    //    kMaxPlanes = 4,
+    class VideoFrame : public rtc::RefCountInterface
+    {
+    public:
+        using ReturnBufferToPoolCallback = void (*)(std::unique_ptr<GpuMemoryBuffer>);
 
-    //    kYPlane = 0,
-    //    kARGBPlane = kYPlane,
-    //    kUPlane = 1,
-    //    kUVPlane = kUPlane,
-    //    kVPlane = 2,
-    //    kAPlane = 3,
-    //};
+        VideoFrame() = delete;
+        VideoFrame(const VideoFrame&) = delete;
+        VideoFrame& operator=(const VideoFrame&) = delete;
 
-    VideoFrame() = delete;
-    VideoFrame(const VideoFrame&) = delete;
-    VideoFrame& operator=(const VideoFrame&) = delete;
+        Size size() const { return size_; }
+        TimeDelta timestamp() const { return timestamp_; }
+        void set_timestamp(TimeDelta timestamp) { timestamp_ = timestamp; }
 
-    Size size() const { return size_; }
-    TimeDelta timestamp() const { return timestamp_; }
-    void set_timestamp(TimeDelta timestamp) { timestamp_ = timestamp; }
+        GpuMemoryBuffer* GetGpuMemoryBuffer() const;
+        bool HasGpuMemoryBuffer() const;
 
-    GpuMemoryBuffer* GetGpuMemoryBuffer() const;
-    bool HasGpuMemoryBuffer() const;
+        static rtc::scoped_refptr<VideoFrame> WrapExternalGpuMemoryBuffer(
+            const Size& size,
+            std::unique_ptr<GpuMemoryBuffer> gpu_memory_buffer,
+            ReturnBufferToPoolCallback returnBufferToPoolCallback,
+            TimeDelta timestamp);
+        static rtc::scoped_refptr<VideoFrame>
+        ConvertToMemoryMappedFrame(rtc::scoped_refptr<VideoFrame> video_frame);
 
-    static rtc::scoped_refptr<VideoFrame> WrapExternalGpuMemoryBuffer(
-        const Size& size,
-        //const gfx::Rect& visible_rect,
-        //const gfx::Size& natural_size,
-        std::unique_ptr<GpuMemoryBuffer> gpu_memory_buffer,
-        //const gpu::MailboxHolder(&mailbox_holders)[kMaxPlanes],
-        //ReleaseMailboxAndGpuMemoryBufferCB mailbox_holder_and_gmb_release_cb,
-        TimeDelta timestamp);
-    static rtc::scoped_refptr<VideoFrame> ConvertToMemoryMappedFrame(
-        rtc::scoped_refptr<VideoFrame> video_frame);
-protected:
-    VideoFrame(
-        const Size& size,
-        std::unique_ptr<GpuMemoryBuffer> gpu_memory_buffer,
-        TimeDelta timestamp);
-    virtual ~VideoFrame() {}
-private:
-    Size size_;
-    std::unique_ptr<GpuMemoryBuffer> gpu_memory_buffer_;
-    TimeDelta timestamp_;
-};
+    protected:
+        VideoFrame(
+            const Size& size,
+            std::unique_ptr<GpuMemoryBuffer> gpu_memory_buffer,
+            ReturnBufferToPoolCallback returnBufferToPoolCallback,
+            TimeDelta timestamp);
+        virtual ~VideoFrame();
+
+    private:
+        Size size_;
+        std::unique_ptr<GpuMemoryBuffer> gpu_memory_buffer_;
+        ReturnBufferToPoolCallback returnBufferToPoolCallback_;
+        TimeDelta timestamp_;
+    };
 
 } // end namespace webrtc
 } // end namespace unity
