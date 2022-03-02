4179dbea942a1680baf6b92a208c0a3b3245ee52
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2022-03-02 20:42:14 +0900

    feat: Allow external libraries to access internal members

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -2,3 +2,6 @@
 
 [assembly: InternalsVisibleTo("Unity.WebRTC.RuntimeTests")]
 [assembly: InternalsVisibleTo("Unity.WebRTC.EditorTests")]
+
+// [Note: 2022/03/02] Allow external libraries to access internal members.
+[assembly: InternalsVisibleTo("Unity.WebRTC.UtilityToolkit")]
